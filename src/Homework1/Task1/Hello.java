5a25b82e4620f9321cf3b73bf565340a74a91884
Viktor Shilay <viktor.shilay@gmail.com>
2020-07-05 20:04:11 +0300

    add homework1

---
+++ src/Homework1/Task1/Hello.java
@@ -0,0 +1,7 @@
+package Homework1.Task1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/Homework1/Task2/Hello.java
@@ -0,0 +1,7 @@
+package Homework1.Task2;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0]);
+    }
+}
